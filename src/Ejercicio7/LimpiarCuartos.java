05aced706c9fbc23587f9d7d042f7d2ac07e5386
aleja <aleja@estacion05>
2017-02-28 06:14:16 -0500

    Karel

--- src/Ejercicio7/LimpiarCuartos.java
+++ src/Ejercicio7/LimpiarCuartos.java
@@ -6,6 +6,7 @@ public class LimpiarCuartos {
 
     public static void main(String[] args) {
         City cd = new City(11, 11);
+        cd.setFrameTitle("Ejercicio 7");
         Robot r = new Robot(cd, 9, 5, Direction.NORTH);
         Cuartos c = new Cuartos(cd, r);
         c.Recoger();
